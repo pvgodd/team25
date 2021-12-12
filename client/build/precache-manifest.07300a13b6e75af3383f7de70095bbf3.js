1a607e176c0702c02215ef5f07c39ad6256c0128
pvgodd <mkinhy258@gmail.com>
2021-12-13 03:49:08 +0900

    1st

---
+++ client/build/precache-manifest.07300a13b6e75af3383f7de70095bbf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26553371a207a1bf209e649dc394d159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e5c1e97e60ca56d0b8b",
+    "url": "/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "465352b8c5dcc8f7e262",
+    "url": "/static/css/main.8458616b.chunk.css"
+  },
+  {
+    "revision": "4e5c1e97e60ca56d0b8b",
+    "url": "/static/js/2.e788bd1f.chunk.js"
+  },
+  {
+    "revision": "b8a4308bea39eadde4b9118b7d1441bf",
+    "url": "/static/js/2.e788bd1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465352b8c5dcc8f7e262",
+    "url": "/static/js/main.ba25680b.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
